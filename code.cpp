ba077779cfdb4188556662989234e4eb73443000
Amannadeem19 <nadeemaman608@gmail.com>
2022-11-11 20:20:01 +0500

    update the space optimisation

--- code.cpp
+++ code.cpp
@@ -47,6 +47,23 @@ int solveTab(vector<int> &nums){
     
 }
 
+int solveSpaceOptimisation(vector<int> &nums){
+
+       
+    int n = nums.size();
+    int prev2 = 0;
+    int prev1 = nums[0];
+   for(int i=1; i<n;i++){
+       int include = prev2 + nums[i];
+       int exclude = prev1 + 0;
+       int ans = max(include, exclude);
+       prev2 = prev1;
+       prev1 = ans;
+       
+   }
+           return prev1;
+    
+}
 
 int maximumNonAdjacentSum(vector<int> &nums){
     // Write your code here.
@@ -57,7 +74,10 @@ int maximumNonAdjacentSum(vector<int> &nums){
 //     return solveMem(nums, nums.size()-1, dp);
     
 //     using tabulation
-    return solveTab(nums);
+//     return solveTab(nums);
+//     make space complexity to O(1)
+    return solveSpaceOptimisation(nums);
+    
     
     
 }
